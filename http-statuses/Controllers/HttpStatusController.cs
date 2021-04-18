e75c428a615db917e1824b303c2757d36b98bfc2
Daniel Brakke <daniel.brakke@fjorgedigital.com>
2021-04-18 13:34:34 -0500

    add parsing of MDN status code docs

--- http-statuses/Controllers/HttpStatusController.cs
+++ http-statuses/Controllers/HttpStatusController.cs
@@ -1,6 +1,9 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+using System.Net.Http;
 using System.Net;
+using AngleSharp;
 using System;
 
 namespace http_statuses.Controllers
@@ -41,9 +44,60 @@ namespace http_statuses.Controllers
             return new JsonResult(text);
         }
 
-        public static string GetStatusCodeInfo(int statusCode)
+        public static async Task<string> GetStatusCodeInfo(int statusCode)
         {
-            return statusCode.ToString();
+            var client = new HttpClient();
+            var url = $"https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/{statusCode}";
+            var response = await client.GetAsync(url);
+            var contents = response.Content;
+            var result = await contents.ReadAsStringAsync();
+
+            var context = BrowsingContext.New(Configuration.Default);
+            var document = await context.OpenAsync(req => req.Content(result));
+
+            var html = await context.OpenNewAsync();
+            var statusCodeHtml = document.QuerySelector(".main-page-content");
+
+            if (statusCodeHtml == null)
+            {
+                return @$"
+                    <h1>Sorry, it looks like that status page is broken.</h1>
+                    <a href='{url}' target='_blank'>{url}</a>
+                ";
+            }
+
+            var selectors = new []
+            {
+                "h1",
+                "#status",
+                "#examples",
+                "#specifications"
+            };
+
+            foreach (var selector in selectors)
+            {
+                var node = statusCodeHtml.QuerySelector(selector);
+                var sibling = statusCodeHtml.QuerySelector($"{selector} + div");
+                if (node != null)
+                {
+                    html.Body.AppendChild(node);
+                }
+
+                if (sibling != null)
+                {
+                    html.Body.AppendChild(sibling);
+                }
+            }
+
+            foreach(IHtmlAnchorElement anchor in html.Body.QuerySelectorAll("a"))
+            {
+                if (anchor.Href.Contains("localhost"))
+                {
+                    anchor.OuterHtml = $"<span>{anchor.InnerHtml}</span>";
+                }
+            }
+
+            return html.Body.InnerHtml;
         }
     }
 }
